c912813dedd99b312d096ef20e29ffe8b223fb5e
Patryk <krasowski.p@outlook.com>
2023-02-09 22:02:45 +0100

    changed basic function on arrow function

---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,16 @@
+import './ExpenseDate.css'
+
+const ExpenseDate = props => {
+	const month = props.date.toLocaleString('en-GB', { month: 'long' })
+	const year = props.date.getFullYear()
+	const day = props.date.toLocaleString('en-GB', { day: '2-digit' })
+	return (
+		<div className='expense-date'>
+			<div className='expense-date__month'>{month}</div>
+			<div className='expense-year'>{year}</div>
+			<div className='expense-day'>{day}</div>
+		</div>
+	)
+}
+
+export default ExpenseDate
