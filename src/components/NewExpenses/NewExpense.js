e4337c9da75deb14fa1e6d409b18bf4bb1472adb
Patryk <krasowski.p@outlook.com>
2023-02-10 20:23:17 +0100

    Created new component NewExpense

---
+++ src/components/NewExpenses/NewExpense.js
@@ -0,0 +1,12 @@
+import './NewExpense.css'
+import ExpenseForm from './ExpenseForm'
+
+const NewExpenses = () => {
+	return (
+		<div className='new-expense'>
+			<ExpenseForm />
+		</div>
+	)
+}
+
+export default NewExpenses
