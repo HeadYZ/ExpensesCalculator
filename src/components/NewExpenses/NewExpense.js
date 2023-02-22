e6d726b42564e748e7bf9af569b4abfec7b968d1
Patryk <krasowski.p@outlook.com>
2023-02-22 20:14:24 +0100

    adding a function responsible for displaying the form on click

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -1,18 +1,31 @@
+import { useState } from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
 const NewExpense = props => {
+
+	const [isEditing, setIsEditing] = useState(false)
+
 	const saveExpenseData = enteredExpenseData => {
 		const expenseData = {
 			...enteredExpenseData,
 			id: Math.random().toString(),
 		}
 		props.onAddExpense(expenseData)
+		setIsEditing(false)
+	}
+
+	const startEditHandler = () => {
+		setIsEditing(true)
+	}
+	const endEditHandler = () => {
+		setIsEditing(false)
 	}
 
 	return (
 		<div className='new-expense'>
-			<ExpenseForm onSaveExpenseData={saveExpenseData} />
+		{!isEditing && <button onClick={startEditHandler}>Add New Expense</button>}
+		{	isEditing && <ExpenseForm onSaveExpenseData={saveExpenseData} stopEditing ={endEditHandler} />}
 		</div>
 	)
 }
